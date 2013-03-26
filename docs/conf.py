1d8269061a14752e2e253749d80000c33ae5f0df
Bruno Oliveira <nicoddemus@gmail.com>
2013-03-25 22:15:11 -0300

    using default theme when building in readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -91,12 +91,16 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'agogo'
-html_theme = 'sphinxdoc'
-#html_theme = 'scrolls'
-#html_theme = 'nature'
-#html_theme = 'haiku'
-#html_theme = 'pyramid'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    #html_theme = 'agogo'
+    html_theme = 'sphinxdoc'
+    #html_theme = 'scrolls'
+    #html_theme = 'nature'
+    #html_theme = 'haiku'
+    #html_theme = 'pyramid'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
