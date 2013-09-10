c83665aa10de362643f2aefd0c31b3c02da464ef
Bruno Oliveira <nicoddemus@gmail.com>
2013-09-10 00:31:13 -0300

    bumping release to 1.0.2

--- pytestqt/__init__.py
+++ pytestqt/__init__.py
@@ -27,6 +27,6 @@ interaction, like key presses and mouse clicks::
 '''
 
 # The short X.Y version.
-version = '1.0.1'
+version = '1.0.2'
 # The full version, including alpha/beta/rc tags.
-release = '1.0.1'
+release = '1.0.2'
