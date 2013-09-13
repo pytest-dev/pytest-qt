35f7cc6c1fd026b1c22a054ba129f599b0877a10
Bruno Oliveira <nicoddemus@gmail.com>
2013-09-12 21:45:33 -0300

    implemented tests for mouse events
    this fixes #2.

--- pytestqt/_tests/test_basics.py
+++ pytestqt/_tests/test_basics.py
@@ -3,12 +3,22 @@ import pytest
 
 
 #===================================================================================================
+# fixtures
+#===================================================================================================
+@pytest.fixture
+def event_recorder(qtbot):
+    widget = EventRecorder()
+    qtbot.addWidget(widget)
+    return widget
+
+
+#===================================================================================================
 # testBasics
 #===================================================================================================
 def testBasics(qtbot):
     '''
     Basic test that works more like a sanity check to ensure we are setting up a QApplication
-    properly and are able to display a simple widget. 
+    properly and are able to display a simple event_recorder. 
     '''
     assert type(QtGui.qApp) is QtGui.QApplication
     widget = QtGui.QWidget()
@@ -23,56 +33,78 @@ def testBasics(qtbot):
 #===================================================================================================
 # testKeyEvents
 #===================================================================================================
-def testKeyEvents(qtbot):
+def testKeyEvents(qtbot, event_recorder):
     '''
     Basic key events test.
     '''
+    def extract(key_event):
+        return (
+            key_event.type(),
+            key_event.key(),
+            key_event.text(),
+        )
+        
+    event_recorder.registerEvent(QtGui.QKeyEvent, extract)
     
-    class MyWidget(QtGui.QWidget):
+    qtbot.keyPress(event_recorder, 'a')
+    assert event_recorder.event_data == (QEvent.KeyPress, int(Qt.Key_A), 'a')
+    
+    qtbot.keyRelease(event_recorder, 'a')
+    assert event_recorder.event_data == (QEvent.KeyRelease, int(Qt.Key_A), 'a')
+    
+    
+#===================================================================================================
+# testMouseEvents
+#===================================================================================================
+def testMouseEvents(qtbot, event_recorder):
+    '''
+    Basic mouse events test.
+    '''
+    def extract(mouse_event):
+        return (
+            mouse_event.type(),
+            mouse_event.button(),
+            mouse_event.modifiers(),
+        )
         
-        def __init__(self):
-            QtGui.QWidget.__init__(self)
-            self._reset()
+    event_recorder.registerEvent(QtGui.QMouseEvent, extract)
+    
+    qtbot.mousePress(event_recorder, Qt.LeftButton)
+    assert event_recorder.event_data == (QEvent.MouseButtonPress, Qt.LeftButton, Qt.NoModifier)
+    
+    qtbot.mousePress(event_recorder, Qt.RightButton, Qt.AltModifier)
+    assert event_recorder.event_data == (QEvent.MouseButtonPress, Qt.RightButton, Qt.AltModifier)
+    
+    
+#===================================================================================================
+# EventRecorder
+#===================================================================================================
+class EventRecorder(QtGui.QWidget):
+    '''
+    Widget that records some kind of events sent to it.
+    
+    When this event_recorder receives a registered event (by calling `registerEvent`), it will call
+    the associated *extract* function and hold the return value from the function in the 
+    `event_data` member.
+    '''
         
-        def event(self, ev):
-            if type(ev) is QtGui.QKeyEvent:
-                self._record(ev)
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self._event_types = {}
+        self.event_data = None
+        
+        
+    def registerEvent(self, event_type, extract_func):
+        self._event_types[event_type] = extract_func
+        
+    
+    def event(self, ev):
+        for event_type, extract_func in self._event_types.iteritems():
+            if type(ev) is event_type:
+                self.event_data = extract_func(ev)
                 return True
-            
-            return False
         
-        
-        def _record(self, key_event):
-            self.type = key_event.type()
-            self.key = key_event.key()
-            self.text = key_event.text()
-            self.modifiers = key_event.modifiers()
-            
-            
-        def _reset(self):
-            self.type = None
-            self.key = None
-            self.text = None
-            self.modifiers = None
-            
-            
-        def assertEvent(self, event_type, key, text, modifiers=Qt.NoModifier):
-            assert self.type == event_type
-            assert self.key == key
-            assert self.text == text
-            assert self.modifiers == modifiers
-            self._reset()
-        
-    
-    # create test widget
-    widget = MyWidget()
-    qtbot.addWidget(widget)
-    
-    qtbot.keyPress(widget, 'a')
-    widget.assertEvent(QEvent.KeyPress, int(Qt.Key_A), 'a')
-    
-    qtbot.keyRelease(widget, 'a')
-    widget.assertEvent(QEvent.KeyRelease, int(Qt.Key_A), 'a')
+        return False
     
     
 #===================================================================================================
