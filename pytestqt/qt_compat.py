62696cb216fa532486d31e7edc241278a2160311
Bruno Oliveira <nicoddemus@gmail.com>
2014-07-19 11:43:42 -0300

    Fixing travis build

--- pytestqt/qt_compat.py
+++ pytestqt/qt_compat.py
@@ -68,10 +68,8 @@ else:
         def __getattr__(cls, name):
             if name in ('__file__', '__path__'):
                 return '/dev/null'
-            elif name[0] == name[0].upper():
-                mockType = type(name, (), {})
-                mockType.__module__ = __name__
-                return mockType
+            elif name == '__name__':
+                return name
             else:
                 return Mock()
     
