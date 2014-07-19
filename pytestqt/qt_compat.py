06697211683c2f4531f1ed5ded838716df2bb7b4
Bruno Oliveira <nicoddemus@gmail.com>
2014-07-19 14:48:02 -0300

    Excluding rtd branch from coverage in qt_compat

--- pytestqt/qt_compat.py
+++ pytestqt/qt_compat.py
@@ -54,7 +54,7 @@ if not on_rtd:
     Qt = QtCore.Qt
     QEvent = QtCore.QEvent
     
-else:
+else:  # pragma: no cover
     USING_PYSIDE = True
 
     # mock Qt when we are generating documentation at readthedocs.org
