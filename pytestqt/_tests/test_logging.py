6e90e1726aac6a72aa8da341d0d1752b4bbadbbc
Bruno Oliveira <nicoddemus@gmail.com>
2015-04-29 21:05:19 -0300

    Strip spaces after the end of message on tests
    PyQt4 and PySide differ on this aspect, it seems

--- pytestqt/_tests/test_logging.py
+++ pytestqt/_tests/test_logging.py
@@ -55,13 +55,11 @@ def test_qtlog_fixture(qtlog):
     qDebug('this is a DEBUG message')
     qWarning('this is a WARNING message')
     qCritical('this is a CRITICAL message')
-    # note that in the messages below, we have an extra space at the end
-    # added by Qt
-    messages = [(int(m.msg_type), m.msg) for m in qtlog.messages]
+    messages = [(m.msg_type, m.msg.strip()) for m in qtlog.messages]
     assert messages == [
-        (int(QtDebugMsg), 'this is a DEBUG message '),
-        (int(QtWarningMsg), 'this is a WARNING message '),
-        (int(QtCriticalMsg), 'this is a CRITICAL message '),
+        (QtDebugMsg, 'this is a DEBUG message'),
+        (QtWarningMsg, 'this is a WARNING message'),
+        (QtCriticalMsg, 'this is a CRITICAL message'),
     ]
     # `messages` attribute is read-only
     with pytest.raises(AttributeError):
