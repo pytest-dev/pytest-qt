a044d33c1e29a1d283baa6bd24b1c63676b061df
Florian Bruhin <me@the-compiler.org>
2015-05-21 06:41:43 +0200

    Install debugging symbols on Travis

--- install-qt.py
+++ install-qt.py
@@ -15,12 +15,12 @@ py3k = sys.version_info[0] == 3
 if os.environ['PYTEST_QT_API'] in ('pyqt4', 'pyqt5'):
     pyqt_ver = os.environ['PYTEST_QT_API'][-1]
     if py3k:
-        run('sudo apt-get install -qq python3-pyqt%s' % pyqt_ver)
+        run('sudo apt-get install -qq python3-pyqt%s{,-dbg}' % pyqt_ver)
     else:
-        run('sudo apt-get install -qq python-qt%s' % pyqt_ver)
+        run('sudo apt-get install -qq python-qt%s{,-dbg}' % pyqt_ver)
 else:
     if py3k:
-        run('sudo apt-get install -qq python3-pyside')
+        run('sudo apt-get install -qq python3-pyside{,-dbg}')
     else:
         run('sudo apt-get install -qq python-pyside')
 
