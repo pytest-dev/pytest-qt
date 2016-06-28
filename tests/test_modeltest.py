26544253ce11afd8fb962b75823dcae74dd08854
Florian Bruhin <git@the-compiler.org>
2016-06-28 22:12:27 +0200

    Fix super call for Python 2

--- tests/test_modeltest.py
+++ tests/test_modeltest.py
@@ -233,7 +233,7 @@ def test_overridden_methods(qtmodeltester):
     class Model(BasicModel):
 
         def __init__(self, parent=None):
-            super().__init__(parent)
+            super(Model, self).__init__(parent)
             self.row_count_did_run = False
 
         def rowCount(self, parent=None):
