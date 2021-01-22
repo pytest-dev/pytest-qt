34740312e53f6dc3c09e87e755bb12ca03abcdc6
JensHeilmann <jensheilmann96@gmail.com>
2021-01-22 10:58:59 +0100

    added support for pyside6

--- src/pytestqt/qtbot.py
+++ src/pytestqt/qtbot.py
@@ -191,7 +191,7 @@ class QtBot:
 
         .. note::
             This function is only available in PyQt5, raising a ``RuntimeError`` if called from
-            ``PySide2``.
+            ``PySide2/6`.
 
         .. note:: This method is also available as ``wait_active`` (pep-8 alias)
         """
@@ -221,7 +221,7 @@ class QtBot:
 
         .. note::
             This function is only available in PyQt5, raising a ``RuntimeError`` if called from
-            ``PySide2``.
+            ``PySide2/6``.
 
         .. note:: This method is also available as ``wait_exposed`` (pep-8 alias)
         """
