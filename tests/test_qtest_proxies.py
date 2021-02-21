39d4a1a799c9c18066f1dc1963ce6dc03bb97e77
TilmanK <tilman.krummeck@googlemail.com>
2021-02-21 19:42:41 +0100

    Marked keyToAscii test to skip on pyside.

--- tests/test_qtest_proxies.py
+++ tests/test_qtest_proxies.py
@@ -1,5 +1,7 @@
 import pytest
 
+from pytestqt.qt_compat import qt_api
+
 
 @pytest.mark.parametrize(
     "expected_method",
@@ -28,6 +30,7 @@ def test_expected_qtest_proxies(qtbot, expected_method):
     assert getattr(qtbot, expected_method).__name__ == expected_method
 
 
+@pytest.mark.skipif(qt_api.pytest_qt_api == "pyside2", reason="PyQt test only")
 def test_keyToAscii_not_available_on_pyqt(testdir):
     """
     Test that qtbot.keyToAscii() is not available on PyQt5 and
