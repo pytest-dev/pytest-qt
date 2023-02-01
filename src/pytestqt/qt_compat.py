a224a138309513af9cd7119e70db1cbda80a9129
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-02-01 20:10:47 +0200

    most tests pass.

--- src/pytestqt/qt_compat.py
+++ src/pytestqt/qt_compat.py
@@ -5,6 +5,7 @@ from qtpy import QtTest, QtCore, QtWidgets, QtQuick, QtQml, QtGui
 
 
 class qt_api:
+    QT_API = qtpy.API
     QtCore = QtCore
     QtTest = QtTest
     QtGui = QtGui
@@ -12,8 +13,12 @@ class qt_api:
     QtQuick = QtQuick
     QtQml = QtQml
     pytest_qt_api = qtpy.API
-    qDebug = QtCore.qDebug
+    Signal = QtCore.Signal
 
-    @staticmethod
-    def is_pyside() -> bool:
-        return qtpy.API in (qtpy.PYSIDE2, qtpy.PYSIDE6)
+    qDebug = QtCore.qDebug
+    qCritical = QtCore.qCritical
+    qInfo = QtCore.qInfo
+    qWarning = QtCore.qWarning
+    is_pyside = qtpy.API in (qtpy.PYSIDE2, qtpy.PYSIDE6)
+
+    is_pyqt = not is_pyside
